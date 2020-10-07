cb5da016198e7ae3281835d7312cfc10ce753bee
Andrey Tedeev <me@andreytedeev.ru>
2020-10-07 18:27:09 +0700

    + Task5

--- csPart1/Part1Lesson5/Program.cs
+++ csPart1/Part1Lesson5/Program.cs
@@ -11,10 +11,11 @@ namespace AndreyTedeev.Part1Lesson5
             Console.WriteLine("Домашнее задание 'Основы языка C#, Урок 5'\n");
 
             IAbstractTask[] tasks = {
-                //new Task1(),
-                //new Task2(),
+                new Task1(),
+                new Task2(),
                 new Task3(),
-                new Task4()
+                new Task4(),
+                new Task5()
             };
 
             foreach (IAbstractTask task in tasks)
