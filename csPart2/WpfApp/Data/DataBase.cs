d84ce530b87d000f03678c3888160def6bdecccf
Andrey Tedeev <andrey.tedeev@outlook.com>
2020-11-13 14:42:40 +0700

    + WpfApp

---
+++ csPart2/WpfApp/Data/DataBase.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using WpfApp.View;
+
+namespace WpfApp.Data
+{
+    public class DataBase
+    {
+       
+        static string EmployeeFileName = "employees.json";
+        
+        static string DepartmentFileName = "departments.json";
+        
+        List<Employee> _employees = new List<Employee>();
+        
+        List<Department> _departments = new List<Department>();
+
+        private DataBase()
+        {}
+
+        static DataBase _dataBase;
+
+        public static DataBase Current 
+        {
+            get => _dataBase ??= new DataBase();
+        }
+
+        public IEnumerable<Employee> Employees { get => _employees; }
+
+        public IEnumerable<Department> Departments { get => _departments; }
+
+        public void Add(Department dept)
+        {
+            dept.Id = (Departments.Count() > 0) ?  Departments.Max(x => x.Id) + 1 : 1;
+            _departments.Add(dept);
+        }
+
+        public void Add(Employee emp)
+        {
+            emp.Id = (Employees.Count() > 0) ? Employees.Max(x => x.Id) + 1 : 1;
+            _employees.Add(emp);
+        }
+
+        public void Remove(Department dept)
+        {
+            int count = (from x in _employees where x.DepartmentId == dept.Id select x.Id).Count();
+            if (count > 0)
+                throw new Exception($"Some Employee record(s) use department {dept.Id}.\n" + 
+                    "Fix those Employee records first.");
+            _departments.Remove(dept);
+        }
+
+        public void Remove(Employee emp)
+        {
+            _employees.Remove(emp);
+        }
+
+        public void Read() {
+            string jsonString;
+            if (File.Exists(EmployeeFileName))
+            {
+                jsonString = File.ReadAllText(EmployeeFileName);
+                _employees = JsonSerializer.Deserialize<List<Employee>>(jsonString);
+            }
+            if (File.Exists(DepartmentFileName))
+            {
+                jsonString = File.ReadAllText(DepartmentFileName);
+                _departments = JsonSerializer.Deserialize<List<Department>>(jsonString);
+            }
+        }
+
+        public void Write()
+        {
+            string jsonString = JsonSerializer.Serialize(_employees);
+            File.WriteAllText("employees.json", jsonString);
+            jsonString = JsonSerializer.Serialize(_departments);
+            File.WriteAllText("departments.json", jsonString);
+        }
+
+    }
+}
---
+++ csPart2/WpfApp/View/EmployeeEditor.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp.Data;
+
+namespace WpfApp.View
+{
+    /// <summary>
+    /// Interaction logic for EmployeeEditor.xaml
+    /// </summary>
+    public partial class EmployeeEditor : UserControl
+    {
+        public EmployeeEditor()
+        {
+            InitializeComponent();
+            cbxDepartment.ItemsSource = DataBase.Current.Departments;
+            cbxDepartment.SelectedIndex = -1;
+            tbxFirstName.Focus();
+        }
+
+        private void tbxFirstName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                tbxLastName.Focus();
+        }
+
+        private void tbxLastName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+                cbxDepartment.Focus();
+        }
+
+        private void CloseDialog(Window parent, bool? result)
+        {
+            parent.DialogResult = result;
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Parent is Window)
+                CloseDialog(this.Parent as Window, true);
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Parent is Window)
+                CloseDialog(this.Parent as Window, false);
+        }
+    }
+}
