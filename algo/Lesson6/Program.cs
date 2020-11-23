d721643baf8857793fcadc0e2ce803e1e8bd2c55
Andrey Tedeev <andrey.tedeev@outlook.com>
2020-11-23 22:03:30 +0700

    + algo Lesson6

---
+++ algo/Lesson6/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Lesson6
+{
+    /// <summary>
+    /// 1. Реализовать простейшую хеш-функцию. На вход функции подается строка, на выходе сумма кодов символов.
+    /// 2. Переписать программу, реализующую двоичное дерево поиска.
+    /// а) Добавить в него обход дерева различными способами;
+    /// б) Реализовать поиск в двоичном дереве поиска;
+    /// в) * Добавить в программу обработку командной строки, с помощью которой можно указывать, 
+    /// из какого файла считывать данные, каким образом обходить дерево.
+    /// 3. *Разработать базу данных студентов из полей «Имя», «Возраст», «Табельный номер», 
+    /// в которой использовать все знания, полученные на уроках.
+    /// Считайте данные в двоичное дерево поиска.
+    /// Реализуйте поиск по какому-нибудь полю базы данных (возраст, вес).
+    /// </summary>
+    class Program
+    {
+
+        public class Node<T> where T : IComparable
+        {
+            Node<T> _parent = null;
+            Node<T> _left = null;
+            Node<T> _right = null;
+
+            public Node<T> Parent { get => _parent; }
+            public Node<T> Left { get => _left; set { _left = value; _left._parent = this; } }
+            public Node<T> Right { get => _right; set { _right = value; _right._parent = this; } }
+            public T Value { get; set; }
+
+            public Node(T value)
+            {
+                Value = value;
+            }
+        }
+
+        public class Tree<T> where T : class, IComparable
+        {
+
+            Node<T> _root = null;
+
+            public Node<T> Root { get => _root; }
+
+            public void Add(T value)
+            {
+                if (_root == null) // если дерево пустое
+                    _root = new Node<T>(value); // добавляем в корень
+                else
+                {
+                    Node<T> node = _root; // начинаем обход с корня
+                    while (node != null) // пока не дошли до конца
+                    {
+                        if (node.Value.CompareTo(value) <= 0) // идем влево
+                        {
+                            if (node.Left is null) // если пусто добавляем
+                            {
+                                node.Left = new Node<T>(value);
+                                return;
+                            }
+                            else // иначе идем дальше по дереву
+                            {
+                                node = node.Left;
+                                continue;
+                            }
+                        }
+                        else // идем вправо
+                        {
+                            if (node.Right is null) // если пусто добавляем
+                            {
+                                node.Right = new Node<T>(value);
+                                return;
+                            }
+                            else // иначе идем дальше по дереву
+                            {
+                                node = node.Right;
+                                continue;
+                            }
+                        }
+                        // TODO: нужно ли балансировать дерево ???
+                        // если да, то вероятно здесь. 
+                        // пока оставлю как есть ...
+                    }
+                }
+            }
+
+            public void Add(IEnumerable<T> data)
+            {
+                foreach (T t in data)
+                    Add(t);
+            }
+
+            public T Find(T template)
+            {
+                if (Root != null) 
+                {
+                    Node<T> node = _root; // начинаем обход с корня
+                    while (node != null) // пока не дошли до конца
+                    {
+                        if (node.Value.CompareTo(template) < 0) // идем влево
+                        {
+                            if (node.Left is null) // если пусто то не нашли
+                                return null;
+                            else // иначе идем дальше по дереву
+                            {
+                                node = node.Left;
+                                continue;
+                            }
+                        }
+                        else if (node.Value.CompareTo(template) > 0) // идем вправо
+                        {
+                            if (node.Right is null) // если пусто то не нашли
+                                return null;
+                            else // иначе идем дальше по дереву
+                            {
+                                node = node.Right;
+                                continue;
+                            }
+                        }
+                        else
+                            return node.Value;
+                    }
+                }
+                return null;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // генерируем три файла
+            List<Student> students;
+            for (int i = 0; i < 3; i++)
+                Student.Write($"students{i + 1}.json", Student.GenerateStudents(30));
+
+            // делаем список файлов и печатаем меню
+            string[] fileNames = Directory.GetFiles(".", "students*.json");
+            Console.WriteLine("Выберите файл данных:");
+            int option;
+            do
+            {
+                option = 0;
+                foreach (string f in fileNames)
+                {
+                    option++;
+                    Console.WriteLine($"{option} : {f}");
+                }
+                Console.WriteLine();
+                Console.WriteLine("0 : Выход");
+            }
+            while (!int.TryParse(Console.ReadLine(), out option) && (option >= 0) && (option < fileNames.Length));
+
+            // выход
+            if (option == 0)
+                return;
+
+            // читаем файл
+            Console.Clear();
+            Console.WriteLine($"Loading file {fileNames[option - 1]}");
+            students = Student.Read(fileNames[option - 1]);
+
+            // будем в дереве искать какого нибудь студента по Age.
+            // другие поля нас не интересуют потому, что 
+            // Student.CompareTo реализован только для Age
+            Student search = new Student
+            {
+                Age = students[students.Count/2].Age
+            };
+
+            // строим дерево
+            Console.WriteLine($"Building tree of {students.Count} items");
+            Tree<Student> tree = new Tree<Student>();
+            tree.Add(students);
+
+            Console.WriteLine($"Performing tree search of Student.Age = {search.Age}");
+            Student found = tree.Find(search);
+            if (found == null)
+                Console.WriteLine("Not found. But it has to be one! Error.");
+            else
+            {
+                Console.WriteLine($"Success! {found}");
+                Console.WriteLine($"Finally... This Student HASH is {Hash(found.ToString())}");
+            }
+        }
+
+        /// <summary>
+        /// Simple Hash implementation
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        private static long Hash(string input)
+        {
+            long result = 0;
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                result += input[i] * input[i + 1];
+            }
+            return result;
+        }
+
+    }
+
+}
---
+++ algo/Lesson6/Student.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace Lesson6
+{
+    
+    public enum Gender
+    {
+        Male = 0,
+        Female = 1
+    }
+
+    public class Student : IComparable
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+
+        public int CompareTo(object other)
+        {
+            if (other is Student)
+                return this.Age.CompareTo((other as Student).Age);
+            throw new ArgumentException("Student is expected");
+        }
+
+        static string[] MF = { "Andrey", "Sergey", "Igor", "Vasily", "Petr", "Slava", "John" };
+        static string[] ML = { "Petrov", "Ivanov", "Sidorov", "Lennon", "Lenin", "Sharapov", "Rasputin" };
+        static string[] FF = { "Anna", "Ekaterina", "Irina", "Alla", "Maria", "Svetlana", "Olga" };
+        static string[] FL = { "Petrova", "Ivanova", "Sidorova", "Katz", "Pugacheva", "Sharapova", "Rasputina" };
+        static Random RANDOM = new Random();
+
+        public static List<Student> GenerateStudents(int quantity = 30)
+        {
+            List<Student> result = new List<Student>();
+            for (int i = 0; i < quantity; i++)
+            {
+                Gender gender = (RANDOM.Next(2, 200) % 2 == 0) ? Gender.Male : Gender.Female;
+                string[] fNames = (gender == Gender.Male) ? MF : FF;
+                string[] lNames = (gender == Gender.Male) ? ML : FL;
+                result.Add(new Student
+                {
+                    Id = i,
+                    FirstName = fNames[RANDOM.Next(0, 7)],
+                    LastName = lNames[RANDOM.Next(0, 7)],
+                    Age = RANDOM.Next(18, 60),
+                    Gender = gender
+                });
+            }
+            return result;
+        }
+
+        public static void Write(string fileName, List<Student> data)
+        {
+            string jsonString = JsonSerializer.Serialize(data);
+            File.WriteAllText(fileName, jsonString);
+        }
+
+        public static List<Student> Read(string fileName)
+        {
+            string jsonString;
+            if (File.Exists(fileName))
+            {
+                jsonString = File.ReadAllText(fileName);
+                return JsonSerializer.Deserialize<List<Student>>(jsonString);
+            }
+            return new List<Student>();
+        }
+
+        public override string ToString() {
+            return $"{{Id: {Id}, Name: {FirstName} {LastName}, Age: {Age}, Gender: {Gender}}}";
+        }
+
+    }
+}
