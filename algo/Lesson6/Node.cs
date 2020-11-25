e597b70ed9a7b504406b36d5654df52665e7ccaf
Andrey Tedeev <andrey.tedeev@outlook.com>
2020-11-26 01:11:53 +0700

    fix Lesson6

---
+++ algo/Lesson6/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson6
+{
+    public class Node<T> where T : IComparable
+    {
+        Node<T> _parent = null;
+        Node<T> _left = null;
+        Node<T> _right = null;
+
+        public Node<T> Parent { get => _parent; }
+        public Node<T> Left { get => _left; set { _left = value; _left._parent = this; } }
+        public Node<T> Right { get => _right; set { _right = value; _right._parent = this; } }
+        public T Value { get; set; }
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+    }
+}
