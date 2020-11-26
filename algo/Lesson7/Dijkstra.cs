40bb6a539f56e227b09f12df5e2bdb908adbaaaf
Andrey Tedeev <andrey.tedeev@outlook.com>
2020-11-26 20:34:37 +0700

    + algo Lesson7

---
+++ algo/Lesson7/Dijkstra.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Lesson5;
+
+namespace Lesson7
+{
+    public class Dijkstra
+    {
+        string[] _nodes;
+        int[,] _matrix;
+
+        public Dijkstra(string[] nodes, int[,] matrix)
+        {
+            if (nodes.Length != matrix.GetUpperBound(0) + 1)
+                throw new ArgumentException("Count of Nodes must match size of Matrix");
+            _nodes = nodes;
+            _matrix = matrix;
+        }
+
+        private int IndexByName(string name)
+        {
+            for (int i = 0; i < _nodes.Length; i++)
+                if (_nodes[i].Equals(name))
+                    return i;
+            return -1;
+        }
+
+        public string[] FindPath(string from, string to)
+        {
+            int start = IndexByName(from);
+            if (start < 0)
+                throw new ArgumentException("from: Unknown Node name");
+            if (from.Equals(to))
+                return new string[] { from };
+            int end = IndexByName(to);
+            if (end < 0)
+                throw new ArgumentException("to: Unknown Node name");
+
+            return Build( Walk(start), end );
+        }
+
+        private int[] Walk(int start)
+        {
+            // init stat array
+            int[] stat = new int[_nodes.Length];
+            for (int i = 0; i < _nodes.Length; i++)
+                stat[i] = int.MaxValue;
+
+            //  begin at the FROM point
+            MyQueue<int> queue = new MyQueue<int>();
+            int node = start;
+            queue.Enqueue(node);
+            stat[node] = 0;
+
+            // walk the graph
+            while (queue.Count > 0)
+            {
+                node = queue.Dequeue();
+                for (int j = 0; j < _matrix.GetUpperBound(0) + 1; j++)
+                {
+                    int temp = ((stat[node] == int.MaxValue) || (_matrix[node, j] == int.MaxValue))
+                        ? int.MaxValue
+                        : stat[node] + _matrix[node, j];
+                    if (stat[j] > temp)
+                    {
+                        stat[j] = temp;
+                        queue.Enqueue(j);
+                    }
+                }
+            }
+            return stat;
+        }
+
+        private string[] Build(int[] stat, int end)
+        {
+            int node = end;
+            MyStack<int> stack = new MyStack<int>();
+            stack.Push(node);
+            int x = stat[node];
+            while (x > 0)
+            {
+                for (int j = 0; j < _matrix.GetUpperBound(0) + 1; j++)
+                {
+                    if (stat[j] == x - _matrix[node, j])
+                    {
+                        stack.Push(j);
+                        x = stat[j];
+                        node = j;
+                        break; // for
+                    }
+                }
+            }
+            string[] result = new string[stack.Count];
+            int index = 0;
+            while (stack.Count > 0)
+            {
+                result[index++] = _nodes[stack.Pop()];
+            }
+            return result;
+        }
+    }
+}
---
+++ algo/Lesson7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+namespace Lesson7
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] nodes = { "X0", "X1", "X2", "X3", "X4", "X5", "X6", "X7" };
+            int m = int.MaxValue;
+            
+            int[,] matrix = {
+                { m, 4, 8, 3, m, m, m, m},
+                { 4, m, 1, m, 8, 5, m, m},
+                { 8, 1, m, 8, 2, m, m, m},
+                { 3, m, 8, m, m, m, 4, m},
+                { m, 8, 2, m, m, 2, m, 5},
+                { m, 5, m, m, 2, m, m, 3},
+                { m, m, m, 4, m, m, m, 2},
+                { m, m, m, m, 5, 3, 2, m}
+            };
+
+            Dijkstra d = new Dijkstra(nodes, matrix);
+            string[] path1 = d.FindPath("X6", "X1");
+            Console.WriteLine($"Path from X6 to X1 : {ToString(path1)}");
+
+            string[] path2 = d.FindPath("X1", "X6");
+            Console.WriteLine($"Path from X1 to X6 : {ToString(path2)}");
+
+            string[] result = (path1.Length < path2.Length) ? path1 : path2;
+            Console.WriteLine($"The shortest route is : {ToString(result)}");
+        }
+
+        static string ToString(string[] array)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < array.Length; i++)
+                sb.Append(array[i] + " ");
+            return sb.ToString();
+        }
+    }
+}
