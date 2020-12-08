46d6e3b3375e962d0150ba5a49db67a5cb3e6419
Andrey Tedeev <andrey.tedeev@outlook.com>
2020-12-08 20:36:33 +0700

    + csPart3 Lesson5

---
+++ csPart3/Lesson5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+using System.Threading;
+
+namespace Lesson5
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Part1.Run();
+            Console.Clear();
+            Part2.Run();
+        }
+
+    }
+}
